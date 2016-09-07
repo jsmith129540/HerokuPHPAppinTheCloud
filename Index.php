8ddb8fe436927c22878002c7bf386e040509ece8
jsmith129540 <jsmith75@southhills.edu>
2016-09-07 15:11:55 -0400

    Added index PHP file

---
+++ Index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Jared@apos;s Web Page</title>
+    </head>
+
+    <body>
+        <h1>My web page on the internet</h1>
+        <?= date("Y"); ?>
+    </body>
+
+</html>
